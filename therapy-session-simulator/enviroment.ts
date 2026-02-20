5f030283c9f8b4116f912f6bf6984e2712d2793a
LAPTOP-0P8HDFVH\maria <mariana.alcazar10@gmail.com>
2026-02-19 21:26:47 -0600

    cambio para deploy

--- therapy-session-simulator/enviroment.ts
+++ therapy-session-simulator/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyCcaZrdePKo5dFZKu1KUCeIWTOOYdYey1k",
     authDomain: "psicologiadc.firebaseapp.com",
@@ -9,6 +9,8 @@ export const environment = {
     appId: "1:23643306129:web:bbc7db11f3656ddb6be730",
     measurementId: "G-21PXS77DHS"
   },
-  apiUrl: 'http://localhost:8000/api/dialogos',
-  usuarioUrl: 'http://localhost:8000/api/usuarios'
+  apiUrl: 'https://simulador-psicologia.onrender.com/api/dialogos',
+  usuarioUrl: 'https://simulador-psicologia.onrender.com/api/usuarios',
+  //apiUrl: 'http://localhost:8000/api/dialogos',
+  //usuarioUrl: 'http://localhost:8000/api/usuarios'
 };
